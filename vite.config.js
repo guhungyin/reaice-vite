593322fbf148683e78c6d0244a915a24bdbd6578
ABOOS <hirosi840101@gmail.com>
2023-09-14 23:07:40 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/reaice/",
+  base: "/reaice-vite/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
